d4163704d292a284c17d7a7c37b565ca4f5562ad
Muneeb Waheed <102057184+1104262@users.noreply.github.com>
2024-02-21 10:32:52 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Calendar from './components/Calendar'
+const App = () => {
+
+  return (
+    <div className="App">
+        <h1>Read Book Everyday 📖 </h1>
+        <h2>☝🏽 At least two pages a day</h2>
+        <Calendar> </Calendar>
+    </div>
+    
+  )
+
+}
+
+export default App 
+
